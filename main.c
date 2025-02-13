aac55bfa33777580af6dfa72e88423323edc7409
kaid <kkrawchuk5@my.bcit.ca>
2025-02-13 08:26:32 -0800

    feat added doctor schedule management

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //the max number of patient to be added to the record
 #define MAX_PATIENT 50
+
+// the number of days in a week: used for the doctors schedule
+#define DAYS 7
+// the number of shifts in a day: morning, afternoon, evening
+#define SHIFTS 3
+
+char doctorSchedule[DAYS][SHIFTS][50];
 
 // structure of the patient record
 typedef struct{
@@ -24,6 +32,10 @@ void displayPatient();
 void dischargePatient();
 void searchPatient();
 
+void initializeSchedule();
+void displaySchedule();
+void assignDoctor();
+void doctorScheduleMenu();
 
 int main(void) {
     //the choice the user would make for the system
@@ -37,7 +49,8 @@ int main(void) {
         printf("2. Display Patient\n");
         printf("3. Discharge Patient\n");
         printf("4. Search Patient\n");
-        printf("5. Exit\n");
+        printf("5. Manage Doctor Schedule\n");
+        printf("6. Exit\n");
         scanf("%d", &choice);
         getchar();
 
@@ -56,7 +69,10 @@ int main(void) {
                 searchPatient();
                 break;
             case 5:
-                printf("Bye");
+                doctorScheduleMenu();
+                break;
+            case 6:
+                printf("Exiting.");
                 return 0;
             default:
                 printf("invalid Choice try again!\n");
@@ -114,3 +130,91 @@ void dischargePatient() {
 void searchPatient() {
     printf("Please enter patient ID:\n");
 }
+
+/* This function initializes the schedule,
+ * the function sets all shifts as 'unassigned' to begin
+ */
+void initializeSchedule() {
+    for (int i = 0; i < DAYS; i++) {
+        for (int j = 0; j < SHIFTS; j++) {
+            strcpy(doctorSchedule[i][j], "Unassigned");
+        }
+    }
+}
+
+// This function displays the doctor's schedule
+void displaySchedule() {
+    char *days[] = {"Monday", "Tuesday", "Wednesday", "Thursday",
+                    "Friday", "Saturday", "Sunday"};
+
+    char *shifts[] = {"Morning", "Afternoon", "Evening"};
+
+    printf("\nDoctor Weekly Schedule: \n");
+    for (int i = 0; i < DAYS; i++) {
+        printf("\n%s\n", days[i]);
+        for (int j = 0; j < SHIFTS; j++) {
+            printf("%s: %s\n", shifts[j], doctorSchedule[i][j]);
+        }
+    }
+}
+
+// Function to assign a doctor to a shift
+void assignDoctor() {
+    int day;
+    int shift;
+    char doctorName[50];
+
+    char *days[] = {"Monday", "Tuesday", "Wednesday", "Thursday",
+                    "Friday", "Saturday", "Sunday"};
+    char *shifts[] = {"Morning", "Afternoon", "Evening"};
+
+    printf("\nSelect a day (1-Monday to 7-Sunday): ");
+    scanf("%d", &day);
+    if (day < 1 || day > 7) {
+        printf("Invalid day!\n");
+        return;
+    }
+
+    printf("Select a shift (1-Morning, 2-Afternoon, 3-Evening): ");
+    scanf("%d", &shift);
+    if (shift < 1 || shift > 3) {
+        printf("Invalid shift!\n");
+        return;
+    }
+    printf("Enter a doctor's name: ");
+    scanf("%s", &doctorName);
+
+    strcpy(doctorSchedule[day - 1][shift - 1], doctorName);
+
+    printf("Doctor %s assigned to %s shift on %s.\n", doctorName, shifts[shift - 1], days[day - 1]);
+}
+
+// The menu for the doctor schedule management
+void doctorScheduleMenu() {
+    int choice;
+
+    do {
+        printf("\n Doctor Schedule Management\n");
+        printf("1. Assign a doctor to a shift.\n");
+        printf("2. View Weekly Schedule\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                assignDoctor();
+                break;
+            case 2:
+                displaySchedule();
+                break;
+            case 3:
+                printf("Exiting Doctor Schedule Management.\n");
+                return;
+            default:
+                printf("Invalid Choice try again!\n");
+        }
+    } while (choice != 3);
+}
+
+
